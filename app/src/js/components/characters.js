cb2284e74938722619272b8a840c77cdf838ff21
Seth Benjamin <animecyc@gmail.com>
2015-03-23 15:19:37 -0400

    Sync characters serially, none of that parallel stuff clogging up the pipes

--- app/src/js/components/characters.js
+++ app/src/js/components/characters.js
@@ -10,15 +10,31 @@ define([
         }) || []
       }, true);
 
-      this.get('characters').forEach(function(character) {
-        character.sync();
-      });
+      this.sync();
     },
 
     view : function() {
       return this.get('characters').map(function(character) {
         return character.view();
       });
+    },
+
+    sync : function() {
+      var self = this;
+      var comps = this.get('characters');
+
+      if(! this.hasOwnProperty('__syncIdx')) {
+        this.__syncIdx = 0;
+      }
+
+      if(this.__syncIdx >= comps.length) {
+        delete this.__syncIdx;
+      } else {
+        comps[this.__syncIdx].sync().then(function() {
+          self.__syncIdx++;
+          self.sync();
+        });
+      }
     }
   });
 });
